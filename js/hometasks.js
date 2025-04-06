a7c41d7443f939e0353f1a69782d9169b237a29d
TeresaBarrero <marbarisi98@gmail.com>
2025-04-06 14:01:56 +0200

    issue nomenclatura ok

--- js/hometasks.js
+++ js/hometasks.js
@@ -65,7 +65,7 @@ const dailyTasks = tasks.filter((task) => {
     return task.date === `${moment.getFullYear()}-${String(moment.getMonth() + 1).padStart(2, '0')}-${String(moment.getDate()).padStart(2, '0')}`
         && task.completed === false && task.important === false
 })
-const MonthTasks = tasks.filter((task) => {
+const monthTasks = tasks.filter((task) => {
     return task.date >= `${moment.getFullYear()}-${String(moment.getMonth() + 1).padStart(2, '0')}-01` &&
         task.date <= `${moment.getFullYear()}-${String(moment.getMonth() + 1).padStart(2, '0')}-${new Date(moment.getFullYear(), moment.getMonth() + 1, 0).getDate()}` &&
         task.date !== `${moment.getFullYear()}-${String(moment.getMonth() + 1).padStart(2, '0')}-${String(moment.getDate()).padStart(2, '0')}`
@@ -136,26 +136,26 @@ function getDailyTask() {
     }))
 }
 function getMonthTask() {
-    const monthcompletedSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="month-Tasks-svg-ok"
+    const monthCompletedSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="month-Tasks-svg-ok"
                         viewBox="0 0 16 16">
                         <path
                             d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
                     </svg>`
-    const monthimportantSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    const monthImportantSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="month-Tasks-important-svg" viewBox="0 0 16 16">
                         <path
                             d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                     </svg>`
-    const monthdeleteSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    const monthDeleteSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="month-Tasks-delete-svg" viewBox="0 0 16 16">
                         <path
                             d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                     </svg>`
-    const completedSvgElement = new DOMParser().parseFromString(monthcompletedSvgCode, 'image/svg+xml').documentElement
-    const importantSvgElement = new DOMParser().parseFromString(monthimportantSvgCode, 'image/svg+xml').documentElement
-    const deleteSvgElement = new DOMParser().parseFromString(monthdeleteSvgCode, 'image/svg+xml').documentElement
+    const completedSvgElement = new DOMParser().parseFromString(monthCompletedSvgCode, 'image/svg+xml').documentElement
+    const importantSvgElement = new DOMParser().parseFromString(monthImportantSvgCode, 'image/svg+xml').documentElement
+    const deleteSvgElement = new DOMParser().parseFromString(monthDeleteSvgCode, 'image/svg+xml').documentElement
 
-    MonthTasks.forEach((task => {
+    monthTasks.forEach((task => {
         const listTask = document.createElement('dd')
         listTask.textContent = task.desc
         listTask.classList.add('Tasks-dd')
@@ -256,18 +256,18 @@ function getCompleteTask() {
 }
 
 function getImportantTask() {
-    const importantcompletedSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="important-Tasks-svg-ok"
+    const importantCompletedSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="important-Tasks-svg-ok"
                         viewBox="0 0 16 16">
                         <path
                             d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
                     </svg>`
-    const importantdeleteSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    const importantDeleteSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="important-Tasks-delete-svg" viewBox="0 0 16 16">
                         <path
                             d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                     </svg>`
-    const completedSvgElement = new DOMParser().parseFromString(importantcompletedSvgCode, 'image/svg+xml').documentElement
-    const deleteSvgElement = new DOMParser().parseFromString(importantdeleteSvgCode, 'image/svg+xml').documentElement
+    const completedSvgElement = new DOMParser().parseFromString(importantCompletedSvgCode, 'image/svg+xml').documentElement
+    const deleteSvgElement = new DOMParser().parseFromString(importantDeleteSvgCode, 'image/svg+xml').documentElement
 
 
     importantTasks.forEach((task => {
